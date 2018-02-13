0cc2fa7b5dfbbdb8e8a911072950ccec19da01d6
Vijay Pillai <vijay.pillai397@gmail.com>
2018-02-12 20:08:12 -0500

    finally done

--- public/image-load.js
+++ public/image-load.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 });
 
 function load_imgs() {
+	$("#gallery").empty();
 	var folder = "./gallery/";
 	//Ajax load adapted from Roko Buljan's answer here: https://stackoverflow.com/questions/18480550/how-to-load-all-the-images-from-one-of-my-folder-into-my-web-page-using-jquery
 	$.ajax({
